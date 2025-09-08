benbrittain/chrono

Migrate to core::error::Error
Stabilized in rust 1.81, behind a feature flag so
as to not bump the current MSRV of 1.61.

src/weekday.rs
@@ -238,6 +238,9 @@ pub struct ParseWeekdayError {
     pub(crate) _dummy: (),
 }
 
+#[cfg(all(not(feature = "std"), feature = "core_error"))]
+impl core::error::Error for ParseMonthError {}
+
 #[cfg(feature = "std")]
 impl std::error::Error for ParseWeekdayError {}
 

strict-rs/strict-chrono

src/weekday.rs
@@ -238,6 +238,9 @@ pub struct ParseWeekdayError {
     pub(crate) _dummy: (),
 }
 
+#[cfg(all(not(feature = "std"), feature = "core-error"))]
+impl core::error::Error for ParseWeekdayError {}
+
 #[cfg(feature = "std")]
 impl std::error::Error for ParseWeekdayError {}
 